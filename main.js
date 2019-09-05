ce304e2dbd0a584a15f712c522dc4edaabdec2e9
Michael Stephens <mikeystephens@hotmail.co.uk>
2019-09-06 00:22:05 +0100

    Have added a basic main implementation so that the script can be called via command line for output to command line or to file. The current implementations work on small sets, however i get a heap out of memory issue when converting the larger sets (10000+) grids to a console safe output. Will split this generation up perhaps with a new function.

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log('Hello World');
+
+
+var multi_grid_gen = require('./multiplication_grid');
+
+var grid_converter = require('./grid_converter');
+
+var fs = require('fs');
+
+//grab the 1st parameter passed to script call
+var prime_size = process.argv[2];
+
+var grid = multi_grid_gen.GenerateMultiplcationGrid(prime_size);
+
+console_friendly_grid = grid_converter.ConvertGridToConsoleOutputArray(grid);
+
+if(process.argv[3] !== undefined)
+{
+  fs.writeFile("./" + process.argv[3], console_friendly_grid.join("\n"), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+  }); 
+
+}
+else
+{
+  for(var i = 0; i < console_friendly_grid.length; ++i)
+  {
+    console.log(console_friendly_grid[i]);
+  }
+}
