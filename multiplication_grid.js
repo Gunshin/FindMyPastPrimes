97beb3112faf825f15b06c0f5a721da490d3e5b3
Michael Stephens <mikeystephens@hotmail.co.uk>
2019-09-05 23:18:53 +0100

    Added implementation for multiplication grid. Returns 2d array of the values

--- multiplication_grid.js
+++ multiplication_grid.js
@@ -1,7 +1,27 @@
 
+var prime_gen = require('./prime_gen');
+
 function GenerateMultiplcationGrid( n )
 {
-  throw Error(["Not Implemented"]);
+  if(n < 1)
+  {
+    return [];
+  }
+  
+  primes = prime_gen.GenerateNPrimes(n);
+  primes_multiplication_grid = [primes];
+  for(var i = 0; i < n; ++i)
+  {
+    grid_row = [primes[i]];
+    for(var j = 0; j < n; ++j)
+    {
+      grid_row.push(primes[i] * primes[j]);
+    }
+    primes_multiplication_grid.push(grid_row);
+  }
+  
+  return primes_multiplication_grid;
+  
 }
 
 module.exports.GenerateMultiplcationGrid = GenerateMultiplcationGrid;
